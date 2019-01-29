16566c39c26c11fac62ea76a813a51cc5b5a21d7
Aid Idrizovic <aid.idrizovic@kognition.ai>
2019-01-28 22:42:15 -0600

    added Scheduled eventing

--- scheduler.java
+++ scheduler.java
@@ -1,4 +1,6 @@
 package main.java;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class scheduler {
 
@@ -11,7 +13,22 @@ public class scheduler {
         System.out.println(lightBulb.getState()? "On" : "Off");
         powerStrip = [new outlet(), new outlet()];
         System.out.println(powerStrip[0].getState()? "On" : "Off", powerStrip[1].getState()? "On" : "Off");
+    }
     
+    private static Date getDate(int hour, int minute, int second)
+    {
+        Calendar today = Calendar.getInstance();
+        today.set(Calendar.HOUR_OF_DAY, hour);
+        today.set(Calendar.MINUTE, minute);
+        today.set(Calendar.SECOND, second);
+        
+        return today;
+    }
+    
+    public void schedule(Date date, Callable<T> func)
+    {
+        Timer timer = new Timer();
+        timer.schedule(func.call(), today.getTime(), TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS));
     }
 
     public void switchPower()
